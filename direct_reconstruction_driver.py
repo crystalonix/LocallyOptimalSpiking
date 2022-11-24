2f0355a9868a68331d2f1e696e1b72bfe37df34d
crystalonix <anik.chatto@gmail.com>
2022-11-24 02:52:34 -0500

    run config for grape 11/24

--- direct_reconstruction_driver.py
+++ direct_reconstruction_driver.py
@@ -12,7 +12,7 @@ import reconstruction_manager
 import signal_utils
 import wav_file_handler
 
-sample_numbers = [i for i in range(200, 400)]
+sample_numbers = [i for i in range(400, 600)]
 # [i for i in range(9, 30)]
 sample_len = 100000
 snip_len = 10000
