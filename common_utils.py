55373b55de6eeab502d63efeab5e5c9358a4cbc8
Anik Chattopadhyay <anikchattopadhyay@Aniks-MacBook-Air.local>
2025-02-03 23:58:43 -0500

    torch backward compatibility fix for the servers

--- common_utils.py
+++ common_utils.py
@@ -118,6 +118,9 @@ def solve_for_coefficients_by_torch(P, T):
     size = len(T)
     P = torch.tensor(P, dtype=torch.float64)
     T = torch.tensor(T, dtype=torch.float64)
+    server_flag = 1
+    if server_flag == 1:
+        return torch.lstsq(T, P).solution
     return torch.linalg.lstsq(P, T).solution
 
 
